703268e6e96ed3433b3e2bd4dc6c78a1d55aad2a
Michael Sjoeberg <mail@michaelsjoeberg.com>
2018-10-24 01:40:28 +0200

    scalatra

--- scalatra-app/test-app/src/main/scala/com/github/michaelsjoeberg/test/Test.scala
+++ scalatra-app/test-app/src/main/scala/com/github/michaelsjoeberg/test/Test.scala
@@ -4,8 +4,12 @@ import org.scalatra._
 
 class Test extends ScalatraServlet {
 
-  get("/") {
-    views.html.hello()
-  }
+    get("/") {
+        views.html.hello()
+    }
+
+    get("/test") {
+        views.html.hello()
+    }
 
 }

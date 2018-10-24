2989d47572d5a3c3b48a106dd964c3b4f4583b42
Michael Sjoeberg <mail@michaelsjoeberg.com>
2018-10-24 19:44:45 +0200

    scalatra app

--- scalatra-app/scalatra-app/src/main/scala/test/Main.scala
+++ scalatra-app/scalatra-app/src/main/scala/test/Main.scala
@@ -1,11 +1,27 @@
 package test
 
 import org.scalatra._
+import servlet.{MultipartConfig, SizeConstraintExceededException, FileUploadSupport}
 
-class Main extends ScalatraServlet {
+class Main extends ScalatraServlet with FileUploadSupport with FlashMapSupport {
+    configureMultipartHandling(MultipartConfig(maxFileSize = Some(3*1024*1024)))
 
-  get("/") {
-    views.html.hello()
-  }
+    get("/") {
+        val content = "No content."
 
+        views.html.hello(content)
+    }
+
+    post("/") {
+        fileParams.get("file") match {
+            case Some(file) =>
+                Ok(file.get(), Map(
+                    "Content-Type"        -> (file.contentType.getOrElse("application/octet-stream")),
+                    "Content-Disposition" -> ("attachment; filename=\"" + file.name + "\"")
+                ))
+
+            case None =>
+                BadRequest(views.html.hello("No file."))
+        }
+    }
 }

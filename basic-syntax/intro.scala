26cb7410a1672b6e40874479d11b5d22f74c76fd
Michael Sjoeberg <mail@michaelsjoeberg.com>
2018-11-05 20:09:32 +0100

    updated intro.scala

--- basic-syntax/intro.scala
+++ basic-syntax/intro.scala
@@ -1,3 +1,5 @@
+// filename.scala
+
 object Main {
     // do something
 
@@ -6,8 +8,19 @@ object Main {
     }
 }
 
-/* '''
+// compile: $ scalac filename.scala
+// run: $ scala filename
+
+// add to build system and run with https://www.sublimetext.com
+{
+    "cmd": ["scala", "$file"],
+    "path": "$PATH:/usr/local/bin",
+    "file_regex": "^(.+):(\\d+): .+: (.+)",
+    "selector": "source.scala"
+}
+
+'''
 Michael Sjoeberg
 2018-11-05
 https://github.com/michaelsjoeberg/scala-playground/blob/master/basic-syntax/intro.scala
-''' */
+'''

19bdb1146f17a92907885b1114beadb839d08d6d
Michael Sjoeberg <mail@michaelsjoeberg.com>
2018-11-05 19:06:49 +0100

    added intro.scala

---
+++ basic-syntax/intro.scala
@@ -0,0 +1,13 @@
+object Main {
+    // do something
+
+    def main(args: Array[String]) {
+        // do something
+    }
+}
+
+// '''
+// Michael Sjoeberg
+// 2018-11-05
+// https://github.com/michaelsjoeberg/scala-playground/blob/master/basic-syntax/intro.scala
+// '''
